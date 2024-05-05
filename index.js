0c72276a65e71b02ec776b09318ce5a050555b9f
deuhsjikl <msjcomentaneo56@hotmail.com>
2024-05-05 12:48:59 +0700

    aptos

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-function productExceptSelf(nums) {
-  const result = [];
-  let product = 1;
-  for (let i = 0; i < nums.length; i++) {
-    result[i] = product;
-    product *= nums[i];
+function lengthOfLIS(nums) {
+  if (nums.length === 0) return 0;
+  const dp = new Array(nums.length).fill(1);
+  let max = 1;
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
+    }
+    max = Math.max(max, dp[i]);
   }
-  product = 1;
-  for (let i = nums.length - 1; i >= 0; i--) {
-    result[i] *= product;
-    product *= nums[i];
-  }
-  return result;
+  return max;
 }
